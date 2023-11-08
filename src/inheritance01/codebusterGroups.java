e704ba111a762a8e4dd6fa6e9a01472f544d1ec2
NAYANQA <nayanqa.om@gmail.com>
2023-11-08 15:46:52 +0000

    week09

---
+++ src/inheritance01/codebusterGroups.java
@@ -0,0 +1,9 @@
+package inheritance01;
+
+public class codebusterGroups extends Codebuster{
+    public static void main(String[] args) {
+
+        Codebuster obj = new Codebuster();
+        obj.groupName();
+    }
+}
---
+++ src/inheritance02/Vehicle.java
@@ -0,0 +1,8 @@
+package inheritance02;
+
+public class Vehicle {
+    public void drive(){
+
+        System.out.println("Repairing a car");
+    }
+}
